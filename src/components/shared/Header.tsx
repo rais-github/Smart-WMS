c6f459f38324ca4f5cfd24cb39f467346d43f5de
Himanshu Rai <himanshu2003rai@gmail.com>
2024-11-18 22:26:23 +0530

    initialize server actions

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -27,6 +27,7 @@ import { Web3Auth } from "@web3auth/modal";
 import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 // import {useMediaQuery} from ";
+import { createUser, getUserByEmail } from "@/utils/db/actions";
 
 const clientId: string = process.env.WEB3_AUTH_CLIENT_ID as string;
 
@@ -66,120 +67,43 @@ const Header = ({
   const [notifications, setNotifications] = useState<Notification[]>([]);
   //   const isMobile = useMediaQuery("(max-width: 768px)");
   const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
+        if (web3auth.connected) {
+          setLoggedIn(true);
+          const user = await web3auth.getUserInfo();
+          setUserInfo(user);
+          if (user.email) {
+            localStorage.setItem("userEmail", user.email);
+            await createUser(user.email, user.name || "Anonymous");
+          }
+        }
+      } catch (error) {
+        console.error("Error initializing web3auth:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    init();
+  }, []);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        if (userInfo && userInfo.email) {
+          const user = await getUserByEmail(userInfo.email);
+        }
+      } catch (error) {
+        console.error("Error fetching notifications:", error);
+      }
+    };
+  }, []);
   return (
-    <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
-      <div className="flex items-center justify-between px-4 py-2">
-        <div className="flex items-center">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="mr-2 md:mr-4"
-            onClick={onMenuClick}
-          >
-            <Menu className="h-6 w-6" />
-          </Button>
-          <Link href="/" className="flex items-center">
-            <Leaf className="h-6 w-6 md:h-8 md:w-8 text-green-500 mr-1 md:mr-2" />
-            <div className="flex flex-col">
-              <span className="font-bold text-base md:text-lg text-gray-800">
-                Zero2Hero
-              </span>
-              <span className="text-[8px] md:text-[10px] text-gray-500 -mt-1">
-                ETHOnline24
-              </span>
-            </div>
-          </Link>
-        </div>
-        {/* {!isMobile && (
-          <div className="flex-1 max-w-xl mx-4">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500"
-              />
-              <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-            </div>
-          </div>
-        )} */}
-        <div className="flex items-center">
-          {/* {isMobile && (
-            <Button variant="ghost" size="icon" className="mr-2">
-              <Search className="h-5 w-5" />
-            </Button>
-          )} */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon" className="mr-2 relative">
-                <Bell className="h-5 w-5" />
-                {notifications.length > 0 && (
-                  <Badge className="absolute -top-1 -right-1 px-1 min-w-[1.2rem] h-5">
-                    {notifications.length}
-                  </Badge>
-                )}
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-64">
-              {notifications.length > 0 ? (
-                notifications.map((notification) => (
-                  //   <DropdownMenuItem
-                  //     key={notification.id}
-                  //     onClick={() => handleNotificationClick(notification.id)}
-                  //   >
-                  <div className="flex flex-col">
-                    {/* <span className="font-medium">{notification.type}</span> */}
-                    <span className="text-sm text-gray-500">
-                      {/* {notification.message} */}
-                    </span>
-                  </div>
-                  //   </DropdownMenuItem>
-                ))
-              ) : (
-                <DropdownMenuItem>No new notifications</DropdownMenuItem>
-              )}
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <div className="mr-2 md:mr-4 flex items-center bg-gray-100 rounded-full px-2 md:px-3 py-1">
-            <Coins className="h-4 w-4 md:h-5 md:w-5 mr-1 text-green-500" />
-            <span className="font-semibold text-sm md:text-base text-gray-800">
-              {balance.toFixed(2)}
-            </span>
-          </div>
-          {/* {!loggedIn ? (
-            <Button
-              onClick={login}
-              className="bg-green-600 hover:bg-green-700 text-white text-sm md:text-base"
-            >
-              Login
-              <LogIn className="ml-1 md:ml-2 h-4 w-4 md:h-5 md:w-5" />
-            </Button>
-          ) : (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="flex items-center"
-                >
-                  <User className="h-5 w-5 mr-1" />
-                  <ChevronDown className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={getUserInfo}>
-                  {userInfo ? userInfo.name : "Fetch User Info"}
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Link href="/settings">Profile</Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem onClick={logout}>Sign Out</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )} */}
-        </div>
-      </div>
-    </header>
+    <header className="bg-white border-b border-gray-200 sticky top-0 z-50"></header>
   );
 };
 
---
+++ src/utils/db/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+import { db } from "./dbConfig";
+import { Users } from "./schema";
+import { eq, sql, and, desc } from "drizzle-orm";
+
+export async function createUser(email: string, name: string) {
+  try {
+    const [user] = await db
+      .insert(Users)
+      .values({ email, name })
+      .returning()
+      .execute();
+    return user;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return null;
+  }
+}
+
+export async function getUserByEmail(email: string) {
+  try {
+    const [user] = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.email, email))
+      .execute();
+    return user;
+  } catch (error) {
+    console.error("Error fetching user by email:", error);
+    return null;
+  }
+}
