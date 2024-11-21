d6c29bfc71dd49e4bfa6bcecb248f6e7303fb33b
Himanshu Rai <himanshu2003rai@gmail.com>
2024-11-21 15:06:52 +0530

    implement sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
-// import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/shared/Sidebar";
 // import "leaflet/dist/leaflet.css";
 import { Toaster } from "react-hot-toast";
 // import { getAvailableRewards, getUserByEmail } from "@/utils/db/actions";
@@ -13,9 +13,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [totalEarnings, setTotalEarnings] = useState(0);
 
@@ -52,8 +52,7 @@ export default function RootLayout({
             totalEarnings={totalEarnings}
           />
           <div className="flex flex-1">
-            Sidebar
-            {/* <Sidebar open={sidebarOpen} /> */}
+            <Sidebar open={sidebarOpen} />
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
               {children}
             </main>
