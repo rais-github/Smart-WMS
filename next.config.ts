00b08877022f19619287f74a6f03cf3de0e4fe2e
Himanshu Rai <himanshu2003rai@gmail.com>
2024-11-21 09:20:56 +0530

    implement mediaQuery hook

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 export default nextConfig;
