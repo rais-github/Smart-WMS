d74495b829fee75551f435717ffe04696c58d601
Himanshu Rai <himanshu2003rai@gmail.com>
2024-11-22 18:49:49 +0530

    implement report page

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   env: {
     WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
     DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,= process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
 };
 
