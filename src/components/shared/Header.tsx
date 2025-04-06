bfd41900188fa4688db1fe4ff1bf3e17a60cc952
Himanshu Rai <himanshu2003rai@gmail.com>
2025-04-06 10:24:54 +0530

    rpc fixed

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -41,7 +41,8 @@ const clientId =
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
   chainId: "0xaa36a7",
-  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  rpcTarget:
+    "https://eth-sepolia.g.alchemy.com/v2/M2MAVOOqGa41mN5TeYW_m76FKtxTU6rc",
   displayName: "Ethereum Sepolia Testnet",
   blockExplorerUrl: "https://sepolia.etherscan.io",
   ticker: "ETH",
